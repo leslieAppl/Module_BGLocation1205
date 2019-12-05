c167572f21582a74c99d949b37244dfd790ccff4
Leslie <leslie.appl@gmail.com>
2019-12-05 10:55:28 +0800

    Initial Commit

---
+++ BGLocation1205/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BGLocation1205
+//
+//  Created by leslie on 12/5/19.
+//  Copyright © 2019 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
